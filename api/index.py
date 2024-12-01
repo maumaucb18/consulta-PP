1989528c926830ab774d82fa05f0e5a6ba93f8e6
maumaucb18 <mauricio.pvargas@gmail.com>
2024-12-01 04:57:07 -0300

    primeiro commit app vercel

--- api/index.py
+++ api/index.py
@@ -1,23 +1,54 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, render_template
 import pandas as pd
-import os
 
+# Inicializar o Flask
 app = Flask(__name__)
 
-# Carregar o arquivo CSV com os dados dos produtos
-csv_file = os.path.join(os.path.dirname(__file__), '../data/produtos_perigosos.csv')
-data_cleaned = pd.read_csv(csv_file)
+# Carregar os dados com tratamento para colunas específicas
+data_path = "relacao_produtos_perigosos.csv"
+data = pd.read_csv(data_path, dtype=str)  # Garantir que os dados sejam strings
 
-@app.route('/api/search', methods=['GET'])
+# Selecionar e renomear colunas
+selected_columns = {
+    "Nº ONU (1)": "Codigo_ONU",
+    "Nome e Descrição (2)": "Nome_Descricao",
+    "Classe ou Subclasse de Risco (3)": "Classe_Subclasse_Risco",
+    "Risco Subsi- diário (4)": "Risco_Subsidiario",
+    "Nº de Risco (5)": "Numero_Risco",
+    "Grupo de Emb. (6)": "Grupo_Embalagem",
+    "Provisões Especiais (7)":"Provisões_especiais",
+   
+
+    
+}
+
+# Filtrar e renomear colunas
+data_cleaned = data[list(selected_columns.keys())].copy()
+data_cleaned.columns = selected_columns.values()
+
+# Substituir valores ausentes por "Não informado"
+data_cleaned.fillna("Não informado", inplace=True)
+
+# Garantir que os códigos ONU estejam no formato correto
+data_cleaned['Codigo_ONU'] = data_cleaned['Codigo_ONU'].str.strip()
+
+# Página inicial com o formulário
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Rota de pesquisa
+@app.route('/search', methods=['GET'])
 def search():
-    query = request.args.get('query', '').upper()
+    query = request.args.get('query', '').upper()  # Converte a entrada para maiúsculas
     filtered_data = data_cleaned[
         data_cleaned['Codigo_ONU'].str.contains(query, na=False) |
         data_cleaned['Nome_Descricao'].str.upper().str.contains(query, na=False)
     ]
     results = filtered_data.to_dict(orient='records')
-    return jsonify(results)
+    return render_template('results.html', results=results, columns=data_cleaned.columns.tolist())
 
-# Para rodar o Flask localmente (não necessário no Vercel)
+
+# Executar o app
 if __name__ == '__main__':
     app.run(debug=True)
